cca20925e300d589882f715491b062c984a8bff0
张海峰 <991150443@qq.com>
2018-05-10 17:15:06 +0800

    Update ViewController.swift

--- ZHFWXImageCollect/ViewController.swift
+++ ZHFWXImageCollect/ViewController.swift
@@ -5,6 +5,13 @@
 //  Created by 张海峰 on 2017/12/20.
 //  Copyright © 2017年 张海峰. All rights reserved.
 //
+// 这是一个tableView与collectionView组合而成的demo，类似微信朋友圈浏览的部分功能，欢迎下载使用
+//  使用说明：
+// 1》引进OC第三方：SDWebImage，MLPhotoBrowser，DACircularProgress。这三个第三方主要实现图片点击浏览缩放功能，及图片保存功能。
+// 2》桥接OC文件
+// 3》根据个人项目需求，比照我的代码搭出框架即可使用。注释比较清晰，需要写的代码也比较清晰简单。（自己写的总代码不超过300行）
+// 4》新手需要注意桥接Build Settings ------> Objective-C Bridging Header  把ZHFWXImageCollect-Bridging-Header.h在你电脑的位置拖进去。
+// 该demo地址：https://github.com/FighterLightning/ZHFWXImageCollect.git
 
 import UIKit
 import YYModel
